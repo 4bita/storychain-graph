66f0029dab8187ce9274abbc325e3c5f26ebcf98
Ilya Tsupryk <ilya_tsupryk@epam.com>
2022-07-29 10:51:04 +0200

    Add test logs

--- src/registry.ts
+++ src/registry.ts
@@ -3,18 +3,20 @@ import {
   storyRegistered
 } from "../generated/Registry/Registry"
 import { Story } from "../generated/schema"
+import { log } from '@graphprotocol/graph-ts'
 
 
 export function handlecandidateRegistered(event: candidateRegistered): void {
+  log.info("Try to register a story", [])
   let event_id = event.transaction.from.toHex()
-  let story = Story.load(event_id)
-  if (!story) {
-    story = new Story(event_id)
-  }
+  let story = new Story("123")
 
   story.profileId = event.params.profileId
   story.pubId = event.params.pubId
+  log.info("Story is loaded. id: '{}', profileId: {}, pubid: {}",
+         [story.id, story.profileId.toString(), story.pubId.toString()])
   story.save()
+  log.info("End saving", [])
 }
 
 export function handlestoryRegistered(event: storyRegistered): void {}
