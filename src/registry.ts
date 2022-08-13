3132c6cdc1848855256ab50f156dfed3a457bd5d
Ilya Tsupryk <ilya_tsupryk@epam.com>
2022-08-14 01:28:28 +0200

    Use block num as event id

--- src/registry.ts
+++ src/registry.ts
@@ -9,17 +9,17 @@ import { log } from '@graphprotocol/graph-ts'
 export function handlecandidateRegistered(event: candidateRegistered): void {}
 
 export function handlestoryRegistered(event: storyRegistered): void {
-  log.info("Try to register a story", [])
-  let event_id = event.transaction.from.toHex()
+  let event_id = event.block.number.toString()
+  log.info("Register a story with id:", [event_id])
   let story = Story.load(event_id)
   if (!story) {
     story = new Story(event_id)
     story.profileId = event.params.profileId
     story.pubId = event.params.pubId
     story.save()
+    log.info('Successfully saved new story', [])
   }
-
-  log.info("Story is loaded. id: '{}', profileId: {}, pubid: {}",
-      [story.id, story.profileId.toString(), story.pubId.toString()])
-  log.info("End saving", [])
+  else {
+    log.info('Story with id: {} already exist', [event_id])
+  }
 }
